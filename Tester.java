0c09d0ff1e1abd4ad9d6b9b146bc9df37be65c82
lakhansamani <lakhan.m.samani@gmail.com>
2015-10-09 13:41:51 +0530

    Modifies xml parsing logic for text and extracts class list

--- Tester.java
+++ Tester.java
@@ -3,9 +3,16 @@
 import java.util.*;
 import java.io.*;
 import java.nio.file.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
 public class Tester {
 
-    public static HashMap<String,HashMap<String,String[]>> extract(String dir){
+    public static HashMap<String,HashMap<String,String[]>> extract(String dir) throws Exception{
         FileInputStream fin;
         File[] faFiles = new File(dir).listFiles();
         String content="";
@@ -17,28 +24,49 @@ public class Tester {
                     extract(file.getAbsolutePath());
                 }
                 else if(file.getName().matches("^(.*?)")&& !file.getName().startsWith(".") && !file.isHidden()){
-                    content=new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
-                    //extracts from <text></text>
-                    String text_content=content.split("<text>")[1].split("</text>")[0];
-                    //removes <p></p> tag
-                    text_content=text_content.replaceAll("\\<[^>]*>","");
-                    //adds text to hashmap
+                    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                    Document doc = dBuilder.parse(new File(file.getAbsolutePath()));
+
+                    NodeList text_node = doc.getElementsByTagName("text");
+                    String text_content=text_node.item(0).getTextContent();
+                    // //adds text to hashmap
                     HashMap<String,String[]> inner_map=new HashMap();
                     
-                    
+                    // //Removing stop words
                     String[] temp=text_content.split("\\s+");
                     String[] rem = new String[]{"of","and","the"};
                     List<String> templ=new ArrayList<String>(Arrays.asList(temp));
                     List<String> reml=new ArrayList<String>(Arrays.asList(rem));
                     templ.removeAll(reml);
-                    String[] fina=new String[templ.size()];
-                    templ.toArray(fina);
+                    String[] final_text=new String[templ.size()];
+                    templ.toArray(final_text);
                     
                     
-                    inner_map.put("text",fina);
+                    inner_map.put("text",final_text);
                     
-                    //TODO: extracts class tag
                     
+                    
+                    doc.getDocumentElement().normalize();
+                    NodeList class_list = doc.getElementsByTagName("codes");
+                    String class_list_string="";
+                    for (int t = 0; t < class_list.getLength(); t++) {
+
+                        Node class_item = class_list.item(t);
+                        Element element = (Element) class_item;
+                        if(element.getAttribute("class").equals("bip:topics:1.0")){
+                            NodeList child = element.getChildNodes();
+                            for(int x=0;x<child.getLength();x++){
+                                Node cNode = child.item(x);
+                                if(cNode.getNodeName().equals("code")){
+                                    Element el = (Element) cNode;
+                                    class_list_string+=el.getAttribute("code")+" ";
+                                }
+                            }
+                        }            
+                    }
+                    inner_map.put("class",class_list_string.split("\\s+"));
+
                     final_map.put(file.getAbsolutePath(),inner_map);
                 }
             }
@@ -49,7 +77,7 @@ public class Tester {
         }
         return final_map;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws Exception{
         HashMap<String,HashMap<String,String[]>> map=extract(args[0]);
         for (Map.Entry entry : map.entrySet()) {
             System.out.println("---------File name:"+entry.getKey() + "--------");
